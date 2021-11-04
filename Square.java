e6e5ddc26eb08ed8cbb763c39eda38b2581cb961
MaybMattie <mattievictor24@gmail.com>
2021-11-04 18:47:33 -0400

    peepee poopoo

---
+++ Square.java
@@ -0,0 +1,11 @@
+public class Square {
+    public boolean mine = false;
+    public boolean revealed = false;
+    int number = 0;
+
+    Square() {
+        boolean mine = false;
+        boolean revealed = false;
+        int number = 0;
+    }
+}
